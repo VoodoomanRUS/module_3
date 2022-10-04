3ca5c7dbeff185743bdcc36004b10c147947ab36
Ablazzing <y2286122@yandex.ru>
2022-10-04 19:26:31 +0300

    добавил домашку №2

--- src/main/java/org/example/module_3/lesson2/Lesson2.java
+++ src/main/java/org/example/module_3/lesson2/Lesson2.java
@@ -2,12 +2,12 @@ package org.example.module_3.lesson2;
 
 public class Lesson2 {
     public static void main(String[] args) {
-        HumanA humanA = HumanB.builder()
+        HumanB humanB = HumanB.builder()
                 .name("Petr")
                 .age(20)
                 .isMale(true)
                 .build();
 
-        System.out.println(humanA);
+        System.out.println(humanB);
     }
 }
